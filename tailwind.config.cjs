fa3a7250418f3a092ef84b69917353b914ac612e
ahmed <ahmedamro3000@gmail.com>
2026-01-22 13:13:17 +0200

    Setup project

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts,css}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
